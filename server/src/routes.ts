1564b286e332606612e9461d98667129f849df1e
Hercules Neves Hamanaka <hamanhrc@schaeffler.com>
2020-08-05 00:15:19 -0300

    feature: Class creation and list implemented

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,78 +1,16 @@
 import express from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHoursToMinutes';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
 
-interface ScheduleItem {
-  week_day: string;
-  from: string;
-  to: string;
-}
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
 
-routes.post('/classes', async (request, response) => {
-  const {
-    name, 
-    avatar, 
-    whatsapp, 
-    bio, 
-    subject, 
-    cost, 
-    schedule 
-  } = request.body;
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
-  const trx = await db.transaction();
-
-  try {
-    // Creating users
-    const insertedUsersIds = await trx('users').insert({
-      name,
-      avatar,
-      whatsapp,
-      bio,
-    });
-  
-    const user_id = insertedUsersIds[0];
-    
-    // Creating classes
-    const insertedClassesIds = await trx('classes').insert({
-      subject,
-      cost,
-      user_id,
-    });
-  
-    const class_id = insertedClassesIds[0];
-  
-    const classSchedules = schedule.map((scheduleItem: ScheduleItem) => {
-      return {
-        class_id,
-        week_day: scheduleItem.week_day,
-        from: convertHourToMinutes(scheduleItem.from),
-        to: convertHourToMinutes(scheduleItem.to),
-      }
-    });
-  
-    await trx('class_schedules').insert(classSchedules);
-
-    await trx.commit();
-
-    return response.status(201).send();
-
-  } catch (err) {
-
-    await trx.rollback();
-    
-    return response.status(400).json({
-      error: 'Unexpected error while creating a new class.'
-    });
-
-  }
-
-  
-
-  
-  
-  response.json().send();
-});
 
 export default routes;
